6b051a4f2785f7dcb96e5abeac5e636ef2ca3fee
Teemu Viikeri <teemu.viikeri@haltu.fi>
2023-12-06 19:43:35 +0200

    Add finished part 1 from day 2.

--- 2023/day2/part1.py
+++ 2023/day2/part1.py
@@ -1,10 +1,27 @@
-import re                                                                                                    
-                                                                                                             
-with open('input.txt', encoding='utf-8') as file:                                                            
-    lines = file.read().splitlines()                                                                         
-    sum_of_game_ids = 0                                                                                      
-    for line in lines:                                                                                       
-        if line:                                                                                             
-            game_id = re.findall("\d")[0]                                                                    
-            sum_of_game_ids += game_id                                                                       
+import re
+
+with open('input.txt', encoding='utf-8') as file:
+    lines = file.read().splitlines()
+    sum_of_game_ids = 0
+    re_max_cubes = {
+        "red": 12,
+        "green": 13,
+        "blue": 14
+    }
+
+    def validate_game(results):
+        for result in results:
+            count, color = result.split(' ')
+            if int(count) > re_max_cubes[color]:
+                return False
+        return True
+
+    for line in lines:
+        if line:
+            game = line[line.index(':') + 2:]
+            results = re.findall("(\d+ (?:red|green|blue))", game)
+            game_is_valid = validate_game(results)
+            if game_is_valid:
+                game_id = re.findall("\d+", line)[0]
+                sum_of_game_ids += int(game_id)
     print(sum_of_game_ids)
